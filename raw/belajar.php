885c39f0bac9444d0210ed0e4acbe38acb4c6fe2
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2025-08-17 10:04:46 +0700

    grimoire

---
+++ raw/belajar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Ini adalah sebuah komentar dari tag html</p>
+    <?php echo "<p>Selamat datang di PHP!</p>"; ?>
+    <p>Ini adalah paragraf kedua.</p>
+    <?php echo "<p>Ini adalah judul utama</p>"; ?>
+</body>
+</html>
