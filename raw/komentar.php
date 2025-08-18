3313511d06f927798bacb9bb168a611efe022682
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2025-08-18 08:57:50 +0700

    u

---
+++ raw/komentar.php
@@ -0,0 +1,11 @@
+<?php
+echo "ini adalah komentar yang tidak ditampilkan di layar web";
+// hai aku adalah komentar bertipe 1 baris
+/* hai kalau aku adalah komentar bertipe banyak baris
+coba lihat ini
+keren kan 
+hehehehe
+aku ga terbatas selagi belum ditutup oleh tag komentarnya
+seperti ini */
+echo "komentar telah berakhir";
+?>
