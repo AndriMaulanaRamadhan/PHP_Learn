33a4f89efd69704b1bea5d93be35be6b112ec205
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2025-08-18 08:34:04 +0700

    u

---
+++ raw/mixed_tag_echo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>ini adalah sebuah kalimat yang didalamnya ada <?php echo "tag echo"; ?></p>
+    <p>dan ini adalah kalimat yang pake mixed tag echo <?= "tag echo"; ?></p>
+</body>
+</html>
