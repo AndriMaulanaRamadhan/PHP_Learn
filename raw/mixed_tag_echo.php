dcf98619db9cb4041454ce77ba5e77fe3a0e6317
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2025-08-18 08:38:26 +0700

    u

--- raw/mixed_tag_echo.php
+++ raw/mixed_tag_echo.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <p>ini adalah sebuah kalimat yang didalamnya ada <?php echo "tag echo"; ?></p>
-    <p>dan ini adalah kalimat yang pake mixed tag echo <?= "tag echo"; ?></p>
+    <p>dan ini adalah kalimat yang pake mixed <?= "tag echo"; ?></p>
 </body>
 </html>
